76a42fcf9cf55d66b15113b120db4ab01efbb4d6
WilsonVlandia <wvelandia79@gmail.com>
2025-08-23 16:08:38 -0500

    Feat: integrate persistent sessions with Redis, add .env support, improve user model with age validation, and update settings for CSRF & CORS

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, FavoriteViewSet
+from .auth_views import csrf, login_view, logout_view, me
 
 # acepta URLs con o sin slash final
 router = DefaultRouter(trailing_slash=True)
@@ -9,4 +10,9 @@ router = DefaultRouter(trailing_slash=True)
 router.register(r'favorites', FavoriteViewSet, basename='favorite')
 router.register(r'', UserViewSet, basename='user')
 
-urlpatterns = router.urls
+urlpatterns =[
+    path('auth/csrf/', csrf, name='csrf'),
+    path('auth/login/', login_view, name='login'),
+    path('auth/logout/', logout_view, name='logout'),
+    path('auth/me/', me, name='me'),
+] + router.urls
