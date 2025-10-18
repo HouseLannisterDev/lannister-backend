e15e79b3e285a42a0d29465df5a494287f1cb98e
Stiven Pabon <stivenupb230@gmail.com>
2025-10-18 14:52:06 -0500

    ayudasssssssss

--- lannister_news_api/settings.py
+++ lannister_news_api/settings.py
@@ -189,7 +189,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # =========================
 AUTH_USER_MODEL = "users.CustomUser"
 
-...existing code...
 
 # =========================
 # MONGODB
